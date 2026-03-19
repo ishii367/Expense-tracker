239691da6911bec0cf98750138d669aa62d1cf3e
ishii367 <ishita.r.bsm36@outlook.com>
2026-03-20 02:58:31 +0530

    fix db connection

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ def get_db_connection():
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
         database=os.getenv("DB_NAME")
+        port=int(os.getenv("DB_PORT")),
+        ssl_disabled=False
     )
 
 
